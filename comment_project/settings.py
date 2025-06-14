7201aa325a00e0f6b98added6ed39e08422e343d
artem <artemyehorov@gmial.com>
2025-06-14 18:48:45 +0200

    Fix: CORS_ALLOWED_ORIGINS

--- comment_project/settings.py
+++ comment_project/settings.py
@@ -67,9 +67,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+#CORS_ALLOWED_ORIGINS = [
+#    "http://localhost:5173",
+#    "http://127.0.0.1:5173",
+#]
+
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "https://dzen-test-frontend.onrender.com",
 ]
 
 ROOT_URLCONF = 'comment_project.urls'
